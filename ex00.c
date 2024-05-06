bc6a30d76040f81df072583ee1499cea5687c87f
giuliaaaaaaaaaaaaaaaa <giudn08@gmail.com>
2024-05-06 14:55:29 +0000

    giulia

---
+++ ex00.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\n");
+    printf("roma roma roma!!!\n");
+    printf("core<3 de sta città\n");
+    printf("unico grande amore\n");
+    printf("de tanta e tanta gente\n");
+    printf("c'hai fatto innamorààààààà\n");
+    printf("<3\n");
+    printf("\n");
+}
+
+
---
+++ gdn.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main ()
+{
+    //VARIABILI//
+    int a , b ;
+    a=2;
+    b=3;
+    printf("somma: %d\n", a+b);
+
+    printf("differenza: %d\n", a-b);
+
+    printf("divisione: %d\n", a;
+
+    printf("moltiplicazione: %d\n", a*b);
+}
